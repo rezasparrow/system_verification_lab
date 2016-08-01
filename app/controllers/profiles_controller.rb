66ca2902e56664f9fcdb449f00c3c7094b2aef74
rezasadeghikhas@yahoo.com <rezasadeghikhas@yahoo.com>
2016-08-02 04:29:18 +0430

    show user  profile

---
+++ app/controllers/profiles_controller.rb
@@ -0,0 +1,37 @@
+class ProfilesController < ApplicationController
+    before_action :set_profile, only: [:show, :edit, :update, :destroy]
+
+    # GET /profiles
+    # GET /profiles.json
+    def index
+        @profiles = User.all
+    end
+
+    # GET /profiles/1
+    # GET /profiles/1.json
+    def show
+        @projects = @profile.projects
+        @publications = @profile.publications
+    end
+
+    # GET /profiles/new
+    def new
+        @profile = Profile.new
+    end
+
+    # GET /profiles/1/edit
+    def edit
+    end
+
+
+    private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_profile
+        @profile = User.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def profile_params
+        params.fetch(:profile, {})
+    end
+end
