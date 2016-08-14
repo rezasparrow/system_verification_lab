95465d78dedff68cfdc81a58ad42c3e16fedb0a5
reza sadeghi <rezasadeghikhas@yahoo.com>
2016-08-14 21:18:31 +0430

    add new ui to sign in and sign up

--- Gemfile
+++ Gemfile
@@ -1,6 +1,7 @@
 source 'https://rubygems.org'
 
-
+#add role for members
+gem "rolify"
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.2.5.1'
 # Use sqlite3 as the database for Active Record
@@ -13,6 +14,9 @@ gem 'uglifier', '>= 1.3.0'
 gem 'coffee-rails', '~> 4.1.0'
 # See https://github.com/rails/execjs#readme for more supported runtimes
 # gem 'therubyracer', platforms: :ruby
+#use for user avatar
+gem "paperclip", "~> 5.0.0"
+
 
 # Use jquery as the JavaScript library
 gem 'jquery-rails'
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160801223732) do
+ActiveRecord::Schema.define(version: 20160814112408) do
 
   create_table "journals", force: :cascade do |t|
     t.string   "name"
@@ -73,6 +73,18 @@ ActiveRecord::Schema.define(version: 20160801223732) do
 
   add_index "publishers", ["name"], name: "index_publishers_on_name", unique: true
 
+  create_table "roles", force: :cascade do |t|
+    t.string   "name"
+    t.integer  "priority"
+    t.integer  "resource_id"
+    t.string   "resource_type"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "roles", ["name", "resource_type", "resource_id"], name: "index_roles_on_name_and_resource_type_and_resource_id"
+  add_index "roles", ["name"], name: "index_roles_on_name"
+
   create_table "sponsors", force: :cascade do |t|
     t.string   "name"
     t.datetime "created_at", null: false
@@ -109,5 +121,12 @@ ActiveRecord::Schema.define(version: 20160801223732) do
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+
+  create_table "users_roles", id: false, force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "role_id"
+  end
+
+  add_index "users_roles", ["user_id", "role_id"], name: "index_users_roles_on_user_id_and_role_id"
 
 end
