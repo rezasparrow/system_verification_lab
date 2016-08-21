6d0c69202dba55b9f526fb4dd6caa417196595f5
rezasadeghikhas@yahoo.com <rezasadeghikhas@yahoo.com>
2016-08-22 03:24:25 +0430

    add profile of user

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,8 +5,8 @@ class User < ActiveRecord::Base
     rolify
     has_attached_file :avatar,
                       styles: {
-                              medium: "300x300>",
-                              thumb: "100x100>"}, default_url: "/images/:style/missing.png"
+                          medium: "300x300>",
+                          thumb: "100x100>"}, default_url: "/images/:style/missing.jpg"
     validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
 
     # Include default devise modules. Others available are:
@@ -18,5 +18,10 @@ class User < ActiveRecord::Base
     has_many :projects, through: :people_projects
     has_many :user_publications
     has_many :publications, through: :user_publications
+
+
+    def full_name
+        return first_name+ " " + last_name
+    end
 
 end
